334332edb2ad8f0f17af47bd86584477946539f3
gaopengwang <gavin.gp.wang@gmail.com>
2019-01-19 09:39:38 -0800

    canvas and setup layout

--- CelebScope/ViewController.swift
+++ CelebScope/ViewController.swift
@@ -8,13 +8,72 @@
 
 import UIKit
 
+class Canvas : UIView {
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        
+        
+        let startPoint = CGPoint(x: 0, y: 0)
+        let endPoint = CGPoint(x:100  , y: 100)
+        
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setLineWidth(7)
+        
+        context.move(to: startPoint)
+        context.addLine(to: endPoint)
+        
+        context.strokePath()
+    }
+}
+
 class ViewController: UIViewController {
 
+    
+    let canvas:Canvas = {
+        let canvas = Canvas()
+        canvas.backgroundColor = UIColor.black
+        canvas.alpha = 0.2
+        return canvas
+    } ()
+    
+    let photoView: UIImageView = {
+        let imageView = UIImageView()
+            
+        imageView.image =     UIImage(imageLiteralResourceName: "hongjinbao")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    } ()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        // stack views
+        view.addSubview(photoView)
+        view.addSubview(canvas)
+        
+        setupLayout()
 
+        
+    }
+    
+    private func setupLayout() {
+        
+        canvas.frame = view.frame
+        
+        
+        
+        photoView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        photoView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        photoView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        
+        photoView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
 
 }
 
