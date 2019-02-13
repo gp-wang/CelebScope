42eaad716fe26523a11021b7a703d3420f247da1
gaopengwang <gavin.gp.wang@gmail.com>
2019-02-13 13:53:02 -0800

    汉化demo

--- CelebScope/Utils/Demos.swift
+++ CelebScope/Utils/Demos.swift
@@ -21,9 +21,9 @@ class Demos {
         
         let photo0 = UIImage(imageLiteralResourceName: "demo0")
         let face0_0 = Face(boundingBox: CGRect(x: 782, y: 372, width: 190, height: 190), image: UIImage(imageLiteralResourceName: "demo0_avartar_0").cgImage!)
-        let person0_0 = Person(id: 0, name: "Movie Star Mary", avartar: UIImage(imageLiteralResourceName: "demo0_avartar_0"), birthDate: "1990", deathDate: "2018", birthPlace: "Neverland", imdbId: "nm0000111", bio: "A very famous female movie star", profession: "Actress, Writer, Producer")
+        let person0_0 = Person(id: 0, name: "韩梅梅", avartar: UIImage(imageLiteralResourceName: "demo0_avartar_0"), birthDate: "1990", deathDate: "2018", birthPlace: "上海", imdbId: "nm0000111", bio: "一个很有名的女影星", profession: "演员，作家，制片人")
         let face0_1 = Face(boundingBox: CGRect(x: 1000, y: 324, width: 200, height: 200), image: UIImage(imageLiteralResourceName: "demo0_avartar_1").cgImage!)
-        let person0_1 = Person(id: 0, name: "Movie Star Jack", avartar: UIImage(imageLiteralResourceName: "demo0_avartar_1"), birthDate: "1980", deathDate: nil, birthPlace: "Neverland", imdbId: "nm0000222", bio: "A very famous male movie star", profession: "Actress, Writer, Producer")
+        let person0_1 = Person(id: 0, name: "李雷", avartar: UIImage(imageLiteralResourceName: "demo0_avartar_1"), birthDate: "1980", deathDate: nil, birthPlace: "北京", imdbId: "nm0000222", bio: "一个很有名的男制片人", profession: "演员，作家，制片人")
         
         
         let demo0 = Demo(photo: photo0, identifications: [
@@ -34,9 +34,9 @@ class Demos {
         
         let photo1 = UIImage(imageLiteralResourceName: "demo1")
         let face1_0 = Face(boundingBox: CGRect(x: 2688, y: 387, width: 387, height: 387), image: UIImage(imageLiteralResourceName: "demo1_avartar_0").cgImage!)
-        let person1_0 = Person(id: 0, name: "Movie Star Mike", avartar: UIImage(imageLiteralResourceName: "demo1_avartar_0"), birthDate: "1970", deathDate: "2016", birthPlace: "Neverland", imdbId: "nm0000333", bio: "A very famous male movie producer", profession: "Actress, Writer, Producer")
+        let person1_0 = Person(id: 0, name: "林涛", avartar: UIImage(imageLiteralResourceName: "demo1_avartar_0"), birthDate: "1970", deathDate: "2016", birthPlace: "Neverland", imdbId: "nm0000333", bio: "有名的男演员", profession: "演员，作家，制片人")
         let face1_1 = Face(boundingBox: CGRect(x: 3545, y: 447, width: 374, height: 374), image: UIImage(imageLiteralResourceName: "demo1_avartar_1").cgImage!)
-        let person1_1 = Person(id: 0, name: "Movie Star Jennifer", avartar: UIImage(imageLiteralResourceName: "demo1_avartar_1"), birthDate: "1980", deathDate: nil, birthPlace: "Neverland", imdbId: "nm0000444", bio: "A very famous male movie writer", profession: "Actress, Writer, Producer")
+        let person1_1 = Person(id: 0, name: "魏华", avartar: UIImage(imageLiteralResourceName: "demo1_avartar_1"), birthDate: "1980", deathDate: nil, birthPlace: "Neverland", imdbId: "nm0000444", bio: "出名的剧本作家", profession: "演员，作家，制片人")
         
         
         let demo1 = Demo(photo: photo1, identifications: [
@@ -49,7 +49,7 @@ class Demos {
         // -------------------------
         let photo2 = UIImage(imageLiteralResourceName: "demo2")
         let face2_0 = Face(boundingBox: CGRect(x: 995, y: 296, width: 200, height: 200), image: UIImage(imageLiteralResourceName: "demo2_avartar_0").cgImage!)
-        let person2_0 = Person(id: 0, name: "Movie Star Maggie", avartar: UIImage(imageLiteralResourceName: "demo2_avartar_0"), birthDate: "1970", deathDate: "2016", birthPlace: "Neverland", imdbId: "nm0000333", bio: "A very famous male movie producer", profession: "Actress, Writer, Producer")
+        let person2_0 = Person(id: 0, name: "玛丽", avartar: UIImage(imageLiteralResourceName: "demo2_avartar_0"), birthDate: "1970", deathDate: "2016", birthPlace: "Neverland", imdbId: "nm0000333", bio: "众所周知的女影视明星", profession: "演员，作家，制片人")
 
 
         let demo2 = Demo(photo: photo2, identifications: [
